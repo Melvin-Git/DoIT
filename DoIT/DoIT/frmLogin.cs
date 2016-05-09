0b6a839a840e7215dfeae5ea8fa34d6d1bf9b01b
Melvin Lupp <melvin.lupp@dava.ch>
2016-05-09 16:20:57 +0200

    All Melvin Changes

--- DoIT/DoIT/frmLogin.cs
+++ DoIT/DoIT/frmLogin.cs
@@ -12,9 +12,59 @@ namespace DoIT
 {
     public partial class frmLogin : Form
     {
+        List<User> users = new List<User>();
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string username = txbRegisterUsername.Text;
+            string password1 = txbRegisterPassword.Text;
+            string password2 = txbRegisterPasswordRepeat.Text;
+
+            // Check if Passwords match
+            if(CheckIfPasswordsMatch(password1, password2) == false)
+            {
+                return;
+            }
+
+            if(CheckIfUserExists(username) == true)
+            {
+                return;
+            }
+
+            RegisterUser(txbRegisterUsername.Text, txbRegisterPassword.Text);
+        }
+
+        // Methods
+        private bool CheckIfPasswordsMatch(string pw1, string pw2)
+        {
+            bool match = true;
+
+            if(!pw1.Equals(pw2))
+            {
+                match = false;
+                MessageBox.Show("The passwords you typed in do not match!", "Passwords do not match");
+            }
+
+            return match;
+        }
+
+        private bool CheckIfUserExists(string username)
+        {
+            // Not filled out yet!
+            return false;
+        }
+
+        private void RegisterUser(string username, string password)
+        {
+            var user = new User();
+            user.setUsername(username);
+            user.setPassword(password);
+            user.AddCalendar();
+            users.Add(user);
         }
     }
 }
